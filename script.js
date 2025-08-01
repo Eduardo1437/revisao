107b302e5fb02cb861053314e825bc7c3ebb0ab5
Eduardo1437 <eduardo.costa.santos30@escola.pr.gov.br>
2025-08-01 08:43:45 -0300

    250801

---
+++ script.js
@@ -0,0 +1,7 @@
+function inserirNome(){
+    let nomeUsuario = prompt("Qual o seu nome?");
+    let elemento = document.querySelector("#nomeUsuario");
+    elemento.textContent = nomeUsuario;
+}
+
+inserirNome();
